5ae77a257c54828e318ee8b0b7d8794d39e719ae
machinagod <afonso.ricardo@gmail.com>
2023-12-19 20:59:22 +0000

    remove build files + gitignore tweak

--- dist/schema/NamespaceRef.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// This file is part of cxsd, copyright (c) 2016 BusFaster Ltd.
-// Released under the MIT license, see LICENSE.
-Object.defineProperty(exports, "__esModule", { value: true });
--- dist/schema/exporter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// This file is part of cxsd, copyright (c) 2015-2016 BusFaster Ltd.
-// Released under the MIT license, see LICENSE.
-Object.defineProperty(exports, "__esModule", { value: true });
-var TS_1 = require("./exporter/TS");
-exports.TS = TS_1.TS;
-var JS_1 = require("./exporter/JS");
-exports.JS = JS_1.JS;
